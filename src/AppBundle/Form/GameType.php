ac0271c9940bb8284f86cb80f3fd08a08b92b0f3
Jordi Freixa <jordi.freixa11@gmail.com>
2017-02-13 20:20:49 +0100

    Entity working and forms

---
+++ src/AppBundle/Form/GameType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Role;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RoleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('role')
+            ->add('label')
+            ->add('save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Role::class,
+        ));
+    }
+}
---
+++ src/AppBundle/Form/ResultType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Game;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GameType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('uri')
+            ->add('image')
+            ->add('save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Game::class,
+        ));
+    }
+}
